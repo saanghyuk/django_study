0805f957042431ce48d0bde95ca053955f1d7d27
Sanghyuk <saanghyuk@gmail.com>
2021-02-22 17:08:53 +0900

    complete register

--- django_practice/fc_community/fcuser/models.py
+++ django_practice/fc_community/fcuser/models.py
@@ -5,11 +5,16 @@ from django.db import models
 
 class Fcuser(models.Model):
     username = models.CharField(max_length=64, verbose_name="사용자명")
+    useremail = models.EmailField(max_length=128, verbose_name="사용자이메일")
     password = models.CharField(max_length=64, verbose_name="비밀번호")
     registered_dttm = models.DateTimeField(
         auto_now_add=True, verbose_name='등록시간')
 
-    # 내가 테이플 이름을 정하고 싶으면 이렇게 쓰면 됨
+    def __str__(self):
+        return self.username
 
+    # 내가 테이플 이름을 정하고 싶으면 이렇게 쓰면 됨
     class Meta:
         db_table = 'fastcampus_fcuser'
+        verbose_name = '고려화학매트 사용자'
+        verbose_name_plural = '고려화학매트 사용자'
--- django_practice/fc_community/fcuser/views.py
+++ django_practice/fc_community/fcuser/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from .models import Fcuser
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
 # Create your views here.
+
+def register(request):
+  if request.method == 'GET':
+    return render(request, 'register.html')
+
+  elif request.method =='POST':
+    username = request.POST.get('username', None)
+    useremail = request.POST.get('useremail', None)
+    password = request.POST.get('password', None)
+    re_password = request.POST.get('re-password', None)
+
+    res_data = {}
+    if not (username and useremail and password and re_password):
+      res_data['error'] = '모든 항목을 입력해주세요.'
+    elif password != re_password:
+      res_data['error'] = '비밀번호가 다릅니다.'
+    else:
+      fcuser = Fcuser(
+        username = username,
+        password = make_password(password),
+        useremail = useremail
+      )
+      fcuser.save()
+
+    return render(request, 'register.html', res_data)
+
