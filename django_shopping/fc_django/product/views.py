93bf7369bdbd88b62fd41c92d1588870af03599c
Sanghyuk <saanghyuk@gmail.com>
2021-03-05 15:33:59 +0900

    deply at GCP finish

--- django_shopping/fc_django/product/views.py
+++ django_shopping/fc_django/product/views.py
@@ -4,10 +4,33 @@ from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
 from order.forms import RegisterForm as OrderForm
+from rest_framework import generics
+from rest_framework import mixins
+from .serializers import ProoductSerializer
 
 from django.utils.decorators import method_decorator
 from fcuser.decorators import admin_required
 # Create your views here.
+
+
+class ProductListAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = ProoductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class ProductDetailAPI(generics.GenericAPIView, mixins.RetrieveModelMixin):
+    serializer_class = ProoductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class ProductList(ListView):
