5a21f8e40e5b49cd96fabb19bbb78b68b47f37c3
Sanghyuk <saanghyuk@gmail.com>
2021-03-15 00:06:33 +0900

    2021.03.14

--- python_advanced/chapter01_03.py
+++ python_advanced/chapter01_03.py
@@ -12,7 +12,7 @@ class Student(object):
   Description: Class, Static, Instance Method
   '''
   # Class Variable
-  tuition = 1.0
+  tuition_per = 1.0
 
   def __init__(self, id, first_name, last_name, email, grade, tuition, gpa):
     self._id = id
@@ -39,7 +39,55 @@ class Student(object):
 
   #Instance Method
   def get_fee_culc(self):
-    return 'After tuition -> Id : {}, fee : {}'.format(self._id, self._tuition * Student.tuition)
+    return 'After tuition -> Id : {}, fee : {}'.format(self._id, self._tuition * Student.tuition_per)
+
+  #Instance Method
+  def __str__(self):
+    return 'Student Info -> name : {}, grade : {}, email : {}'.format(self.full_name(), self._grade, self._email)
+
+  #Class Method
+  #cls라는 첫번째 인자로 Class가 넘어오는 거샹.
+  #cls = Student
+  @classmethod
+  def raise_fee(cls, per):
+    if per <= 1 :
+      print('Please Enter 1 or More')
+      return
+    cls.tuition_per = per
+    print('Succed! tuition increased!')
 
 
+#학생 인스턴스
+student_1 = Student(1, 'Kim', 'Sarang', 'student1@naver.com', '1', 400, 3.5)
+student_2 = Student(1, 'Lee', 'Myungho', 'student2@daum.net', '2', 500, 4.3)
+
+#기본 정보
+print(student_1) #str구현해서 확인이 쉬움
+print(student_2)
+print(student_1.__dict__)
+print(student_2.__dict__)
+
+#전체 정보
+print(student_1.detail_info())
+print(student_2.detail_info())
+
+# 학비 정보(인상 전)
+print(student_1.get_fee())
+print(student_2.get_fee())
+print()
+
+# 학비 인상(클래스 메소드 미사용)
+# 직접 접근하는 것? 너무 안좋다고 말했음.
+# 실수로 1.2대신 1.3누르면 어쩔꺼야.
+Student.tuition_per = 1.2
+print(student_1.get_fee_culc())
+print(student_2.get_fee_culc())
+print()
+Student.tuition_per = 1.0 # reset용 code
+
+# 학비 인상(클래스 메소드 사용)
+Student.raise_fee(1)
+Student.raise_fee(1.5)
+print(student_1.get_fee_culc())
+print(student_2.get_fee_culc())
 
